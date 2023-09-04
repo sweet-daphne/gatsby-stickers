7b2b9208d6c66e7d51709fc819c6808da8abb07a
suelrin <dancerelrin@gmail.com>
2023-09-05 07:20:45 +0900

    layouts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
+import Layout from '../components/Layout';
+import Seo from '../components/Seo';
 
-const IndexPage = () => {
+export default function IndexPage() {
   return (
-    <div>
-      <h1>Welcome to DevStickers 🚛</h1>
-    </div>
+    <Layout title="Welcome to DevStickers 🚛">
+      <div></div>
+    </Layout>
   );
-};
+}
 
-export default IndexPage;
+export const Head = () => <Seo title="Home" />;
