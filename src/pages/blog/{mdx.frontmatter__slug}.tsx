2a9859e0e1ace24408c668941f817b5b2b459dd4
suelrin <dancerelrin@gmail.com>
2023-09-12 19:18:05 +0900

    blog post

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import { graphql } from 'gatsby';
+import Seo from '../../components/Seo';
 
-export default function BlogPos() {
+interface IBlogPostProps {
+  data: Queries.PostDetailQuery;
+  children: any;
+}
+
+export default function BlogPos({ data, children }: IBlogPostProps) {
   return (
-    <Layout title="Blog Post">
-      <div />
+    <Layout title="">
+      <div>{children}</div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      id
+      body
+      frontmatter {
+        author
+        category
+        date
+        title
+        slug
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: IBlogPostProps) => (
+  <Seo title={data.mdx?.frontmatter?.title!} />
+);
